38a9000be96fa65a0ff1a8fd16cebf82b70bfe16
devteamvicky <devteamvicky@gmail.com>
2023-10-17 16:01:05 +0530

    this commit will Calucate average of arrays

--- src/test/java/Arrays/CalculateAverafe.java
+++ src/test/java/Arrays/CalculateAverafe.java
@@ -1,5 +1,18 @@
 package Arrays;
 
 public class CalculateAverafe {
+	public static void main(String[] args) {
 
+		int[] number = { 10, 20, 40, 50, 70 };
+		int sum = 0;
+		int count = 0;
+		for (int i = 0; i < number.length; i++) {
+			sum += number[i];
+			count++;
+		}
+		System.out.println("Total sum of element in the arrays :" + sum);
+		System.out.println("Total count of element in the arrays :" + count);
+		float average = sum / count;
+		System.out.println("Averge of element in the Array : " + average);
+	}
 }
